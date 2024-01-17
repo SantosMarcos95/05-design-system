e9cb178b946dc3417a39d2890c283032c3330089
santosmarcos95 <marcos.p.santoos@gmail.com>
2024-01-17 20:55:44 -0300

    Err: Resolução de bus

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,11 +1,9 @@
-import "@storybook/addon-a11y";
 module.exports = {
   stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
   ],
   framework: "@storybook/react",
   core: {
